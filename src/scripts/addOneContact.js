533c6bdc723dcfb55541e03cbb67e9f973ec68a4
denys-filiuk <filiuk_dp@ukr.net>
2025-06-30 19:05:23 +0300

    fix add contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,9 +1,10 @@
+import { createFakeContact } from '../utils/createFakeContact.js';
 import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
-export const addOneContact = async (newContact) => {
+export const addOneContact = async () => {
   const contacts = await readContacts();
-  contacts.push(newContact);
+  contacts.push(createFakeContact());
   await writeContacts(contacts);
 };
 
